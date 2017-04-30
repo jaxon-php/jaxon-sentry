c08408a73e5eee774bae02739766849292c37641
feuzeu <thierry.feuzeu@gmail.com>
2017-04-30 11:48:18 -0400

    Added classes.

---
+++ src/Sentry.php
@@ -0,0 +1,430 @@
+<?php
+
+namespace Jaxon\Sentry;
+
+use Jaxon\Jaxon;
+use Jaxon\Sentry\Classes\Base;
+use Jaxon\Utils\Container;
+use Jaxon\Utils\Traits\Config;
+use Jaxon\Utils\Traits\View;
+use Jaxon\Utils\Traits\Session;
+use Jaxon\Utils\Traits\Manager;
+use Jaxon\Utils\Traits\Event;
+use Jaxon\Utils\Traits\Validator;
+
+use stdClass, Exception;
+
+class Sentry
+{
+    use Config, View, Session, Manager, Event, Validator;
+
+    protected $xBeforeCallback = null;
+    protected $xAfterCallback = null;
+    protected $xInitCallback = null;
+    protected $xInvalidCallback = null;
+    protected $xErrorCallback = null;
+
+    // Requested class and method
+    private $xRequestObject = null;
+    private $sRequestMethod = null;
+
+    protected $appConfig = null;
+    protected $xResponse = null;
+
+    protected $aViewRenderers = array();
+    protected $aViewNamespaces = array();
+
+    /**
+     * Setup the library.
+     *
+     * @return void
+     */
+    public function setup()
+    {
+        // Create the Jaxon response
+        $this->xResponse = jaxon()->getResponse();
+    }
+
+    /**
+     * Get the Jaxon response.
+     *
+     * @return Response
+     */
+    public function ajaxResponse()
+    {
+        return $this->xResponse;
+    }
+
+    /**
+     * Add a class namespace.
+     *
+     * @param string            $sDirectory             The path to the directory
+     * @param string            $sNamespace             The associated namespace
+     * @param string            $sSeparator             The character to use as separator in javascript class names
+     * @param array             $aProtected             The functions that are not to be exported
+     *
+     * @return void
+     */
+    public function addClassNamespace($sDirectory, $sNamespace, $sSeparator = '.', array $aProtected = array())
+    {
+        // Valid separator values are '.' and '_'. Any other value is considered as '.'.
+        $sSeparator = trim($sSeparator);
+        if($sSeparator != '_')
+        {
+            $sSeparator = '.';
+        }
+        jaxon()->addClassDir(trim($sDirectory), trim($sNamespace), $sSeparator, $aProtected);
+    }
+
+    /**
+     * ASet the class namespaces.
+     *
+     * @param array             $xAppConfig             The application config options
+     *
+     * @return void
+     */
+    public function setClassNamespaces($xAppConfig)
+    {
+        if($xAppConfig->hasOption('classes') && is_array($xAppConfig->getOption('classes')))
+        {
+            $aNamespaces = $xAppConfig->getOption('classes');
+
+            // The public methods of the base class must not be exported to javascript
+            $protected = array();
+            $baseClass = new \ReflectionClass('\\Jaxon\\Sentry\\Classes\\Base');
+            foreach ($baseClass->getMethods(\ReflectionMethod::IS_PUBLIC) as $xMethod)
+            {
+                $protected[] = $xMethod->getShortName();
+            }
+            foreach($aNamespaces as $aNamespace)
+            {
+                // Check mandatory options
+                if(!key_exists('directory', $aNamespace) || !key_exists('namespace', $aNamespace))
+                {
+                    continue;
+                }
+                // Set the default values for optional parameters
+                if(!key_exists('separator', $aNamespace))
+                {
+                    $aNamespace['separator'] = '.';
+                }
+                if(!key_exists('protected', $aNamespace))
+                {
+                    $aNamespace['protected'] = [];
+                }
+                $this->addClassNamespace($aNamespace['directory'], $aNamespace['namespace'],
+                    $aNamespace['separator'], array_merge($aNamespace['protected'], $protected));
+            }
+        }
+    }
+
+    /**
+     * Add a view namespace, and set the corresponding renderer.
+     *
+     * @param string        $sNamespace         The namespace name
+     * @param string        $sDirectory         The namespace directory
+     * @param string        $sExtension         The extension to append to template names
+     * @param string        $sRenderer          The corresponding renderer name
+     *
+     * @return void
+     */
+    public function addViewNamespace($sNamespace, $sDirectory, $sExtension, $sRenderer)
+    {
+        $aNamespace = array(
+            'namespace' => $sNamespace,
+            'directory' => $sDirectory,
+            'extension' => $sExtension,
+        );
+        if(key_exists($sRenderer, $this->aViewNamespaces))
+        {
+            $this->aViewNamespaces[$sRenderer][] = $aNamespace;
+        }
+        else
+        {
+            $this->aViewNamespaces[$sRenderer] = array($aNamespace);
+        }
+        $this->aViewRenderers[$sNamespace] = $sRenderer;
+    }
+
+    /**
+     * Set the view namespaces.
+     *
+     * @param array             $xAppConfig             The application config options
+     *
+     * @return void
+     */
+    public function setViewNamespaces($xAppConfig)
+    {
+        $sDefaultNamespace = $xAppConfig->getOption('options.views.default', false);
+        if(is_array($namespaces = $xAppConfig->getOptionNames('views')))
+        {
+            foreach($namespaces as $namespace => $option)
+            {
+                // If no default namespace is defined, use the first one as default.
+                if($sDefaultNamespace == false)
+                {
+                    $sDefaultNamespace = $namespace;
+                }
+                // Save the namespace
+                $directory = $xAppConfig->getOption($option . '.directory');
+                $extension = $xAppConfig->getOption($option . '.extension', '');
+                $renderer = $xAppConfig->getOption($option . '.renderer', 'jaxon');
+                $this->addViewNamespace($namespace, $directory, $extension, $renderer);
+            }
+        }
+
+        // Save the view renderers and namespaces in the DI container
+        $this->initViewRenderers($this->aViewRenderers);
+        $this->initViewNamespaces($this->aViewNamespaces, $sDefaultNamespace);
+    }
+
+    /**
+     * Set the Jaxon library default options.
+     *
+     * @return void
+     */
+    public function setLibraryOptions($bExtern, $bMinify, $sJsUri, $sJsDir)
+    {
+        $jaxon = jaxon();
+        // Jaxon library settings
+        if(!$jaxon->hasOption('js.app.extern'))
+        {
+            $jaxon->setOption('js.app.extern', $bExtern);
+        }
+        if(!$jaxon->hasOption('js.app.minify'))
+        {
+            $jaxon->setOption('js.app.minify', $bMinify);
+        }
+        if(!$jaxon->hasOption('js.app.uri'))
+        {
+            $jaxon->setOption('js.app.uri', $sJsUri);
+        }
+        if(!$jaxon->hasOption('js.app.dir'))
+        {
+            $jaxon->setOption('js.app.dir', $sJsDir);
+        }
+
+        // Set the request URI
+        if(!$jaxon->hasOption('core.request.uri'))
+        {
+            $jaxon->setOption('core.request.uri', 'jaxon');
+        }
+    }
+
+    /**
+     * Set the init callback, used to initialise class instances.
+     *
+     * @param  callable         $callable               The callback function
+     * @return void
+     */
+    public function setInitCallback($callable)
+    {
+        $this->xInitCallback = $callable;
+    }
+
+    /**
+     * Set the pre-request processing callback.
+     *
+     * @param  callable         $callable               The callback function
+     * @return void
+     */
+    public function setBeforeCallback($callable)
+    {
+        $this->xBeforeCallback = $callable;
+    }
+
+    /**
+     * Set the post-request processing callback.
+     *
+     * @param  callable         $callable               The callback function
+     * 
+     * @return void
+     */
+    public function setAfterCallback($callable)
+    {
+        $this->xAfterCallback = $callable;
+    }
+
+    /**
+     * Set the processing error callback.
+     *
+     * @param  callable         $callable               The callback function
+     * 
+     * @return void
+     */
+    public function setInvalidCallback($callable)
+    {
+        $this->xInvalidCallback = $callable;
+    }
+
+    /**
+     * Set the processing exception callback.
+     *
+     * @param  callable         $callable               The callback function
+     * 
+     * @return void
+     */
+    public function setErrorCallback($callable)
+    {
+        $this->xErrorCallback = $callable;
+    }
+
+    /**
+     * Initialize a class instance.
+     *
+     * @return void
+     */
+    protected function initInstance(Base $instance)
+    {
+        // Return if the class instance has already been initialized.
+        if(!($instance) || ($instance->response))
+        {
+            return;
+        }
+        // Init the class instance
+        $instance->response = $this->xResponse;
+        if(($this->xInitCallback))
+        {
+            call_user_func_array($this->xInitCallback, array($instance));
+        }
+        $instance->init();
+    }
+
+    /**
+     * Get a class instance.
+     *
+     * @param  string  $classname the class name
+     * 
+     * @return object  The registered instance of the class
+     */
+    public function instance($classname)
+    {
+        // Find the class instance, and register the class if the instance is not found.
+        if(!($instance = jaxon()->getPluginManager()->getRegisteredObject($classname)))
+        {
+            $instance = jaxon()->registerClass($classname, [], true);
+        }
+        if(($instance))
+        {
+            $this->initInstance($instance);
+        }
+        return $instance;
+    }
+
+    /**
+     * Get a Jaxon request to a given class.
+     *
+     * @param  string  $classname the class name
+     * 
+     * @return object  The request to the class
+     */
+    public function request($classname)
+    {
+        $instance = $this->instance($classname);
+        return ($instance != null ? $instance->request() : null);
+    }
+
+    /**
+     * This is the pre-request processing callback passed to the Jaxon library.
+     *
+     * @param  boolean  &$bEndRequest if set to true, the request processing is interrupted.
+     * 
+     * @return object  the Jaxon response
+     */
+    public function onEventBefore(&$bEndRequest)
+    {
+        // Validate the inputs
+        $class = $_POST['jxncls'];
+        $method = $_POST['jxnmthd'];
+        if(!$this->validateClass($class) || !$this->validateMethod($method))
+        {
+            // End the request processing if the input data are not valid.
+            // Todo: write an error message in the response
+            $bEndRequest = true;
+            return $this->xResponse;
+        }
+        // Instanciate the class. This will include the required file.
+        $this->xRequestObject = $this->instance($class);
+        $this->sRequestMethod = $method;
+        if(!$this->xRequestObject)
+        {
+            // End the request processing if the class cannot be found.
+            // Todo: write an error message in the response
+            $bEndRequest = true;
+            return $this->xResponse;
+        }
+
+        // Call the user defined callback
+        if(($this->xBeforeCallback))
+        {
+            call_user_func_array($this->xBeforeCallback, array($this->xRequestObject, $this->sRequestMethod, &$bEndRequest));
+        }
+        return $this->xResponse;
+    }
+
+    /**
+     * This is the post-request processing callback passed to the Jaxon library.
+     *
+     * @return object  the Jaxon response
+     */
+    public function onEventAfter()
+    {
+        if(($this->xAfterCallback))
+        {
+            call_user_func_array($this->xAfterCallback, array($this->xRequestObject, $this->sRequestMethod));
+        }
+        return $this->xResponse;
+    }
+
+    /**
+     * This callback is called whenever an invalid request is processed.
+     *
+     * @return object  the Jaxon response
+     */
+    public function onEventInvalid($sMessage)
+    {
+        if(($this->xInvalidCallback))
+        {
+            call_user_func_array($this->xInvalidCallback, array($this->xResponse, $sMessage));
+        }
+        return $this->xResponse;
+    }
+
+    /**
+     * This callback is called whenever an invalid request is processed.
+     *
+     * @return object  the Jaxon response
+     */
+    public function onEventError(Exception $e)
+    {
+        if(($this->xErrorCallback))
+        {
+            call_user_func_array($this->xErrorCallback, array($this->xResponse, $e));
+        }
+        else
+        {
+            throw $e;
+        }
+        return $this->xResponse;
+    }
+
+    /**
+     * Process the current Jaxon request.
+     *
+     * @return void
+     */
+    public function processRequest()
+    {
+        // Process Jaxon Request
+        $jaxon = jaxon();
+        if($jaxon->canProcessRequest())
+        {
+            $jaxon->register(Jaxon::PROCESSING_EVENT, Jaxon::PROCESSING_EVENT_BEFORE, array($this, 'onEventBefore'));
+            $jaxon->register(Jaxon::PROCESSING_EVENT, Jaxon::PROCESSING_EVENT_AFTER, array($this, 'onEventAfter'));
+            $jaxon->register(Jaxon::PROCESSING_EVENT, Jaxon::PROCESSING_EVENT_INVALID, array($this, 'onEventInvalid'));
+            $jaxon->register(Jaxon::PROCESSING_EVENT, Jaxon::PROCESSING_EVENT_ERROR, array($this, 'onEventError'));
+            // Traiter la requete
+            $jaxon->processRequest();
+        }
+    }
+}
---
+++ src/Traits/Armada.php
@@ -0,0 +1,267 @@
+<?php
+
+namespace Jaxon\Sentry\Traits;
+
+use Jaxon\Jaxon;
+use Jaxon\Sentry\Classes\Base;
+use Jaxon\Utils\Container;
+use Jaxon\Utils\Traits\Config;
+use Jaxon\Utils\Traits\View;
+use Jaxon\Utils\Traits\Session;
+use Jaxon\Utils\Traits\Manager;
+use Jaxon\Utils\Traits\Event;
+use Jaxon\Utils\Traits\Validator;
+
+use stdClass, Exception;
+
+trait Armada
+{
+    protected $bSetupCalled = false;
+
+    protected $appConfig = null;
+
+    /**
+     * Set the module specific options for the Jaxon library.
+     *
+     * @return void
+     */
+    abstract protected function jaxonSetup();
+
+    /**
+     * Set the module specific options for the Jaxon library.
+     *
+     * @return void
+     */
+    abstract protected function jaxonCheck();
+
+    /**
+     * Wrap the Jaxon response into an HTTP response and send it back to the browser.
+     *
+     * @param  $code        The HTTP Response code
+     *
+     * @return HTTP Response
+     */
+    abstract public function httpResponse($code = '200');
+
+    /**
+     * Wraps the module/package/bundle setup method.
+     *
+     * @return void
+     */
+    private function _jaxonSetup()
+    {
+        if(($this->bSetupCalled))
+        {
+            return;
+        }
+
+        $jaxon = jaxon();
+        $sentry = $jaxon->sentry();
+        // Use the Composer autoloader. It's important to call this before triggers and callbacks.
+        $jaxon->useComposerAutoloader();
+
+        // Set this object as the Armada in the DI container.
+        // Now it will be returned by a call to jaxon()->module().
+        if(get_class($this) != 'Jaxon\\Armada\\Armada')
+        {
+            Container::getInstance()->setArmada($this);
+        }
+
+        // Event before setting up the module
+        $sentry->triggerEvent('pre.setup');
+
+        // Setup the Sentry library
+        $sentry->setup();
+
+        // Set the module/package/bundle specific specific options
+        $this->jaxonSetup();
+
+        // Event before the module has set the config
+        $sentry->triggerEvent('pre.config');
+
+        // Jaxon application settings
+        $sentry->setClassNamespaces($this->appConfig);
+
+        // Save the view namespaces
+        $sentry->setViewNamespaces($this->appConfig);
+
+        // Event after the module has read the config
+        $sentry->triggerEvent('post.config');
+
+        // Event before checking the module
+        $sentry->triggerEvent('pre.check');
+
+        $this->jaxonCheck();
+
+        // Event after checking the module
+        $sentry->triggerEvent('post.check');
+
+        // Event after setting up the module
+        $sentry->triggerEvent('post.setup');
+
+        $this->bSetupCalled = true;
+    }
+
+    /**
+     * Register the Jaxon classes.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->_jaxonSetup();
+        jaxon()->registerClasses();
+    }
+
+    /**
+     * Get a Jaxon request to a given class.
+     *
+     * @param  string  $classname the class name
+     * 
+     * @return object  The request to the class
+     */
+    public function request($classname)
+    {
+        return jaxon()->sentry()->request($classname);
+    }
+
+    /**
+     * Get a plugin instance.
+     *
+     * @param  string  $name the plugin name
+     * 
+     * @return object  The plugin instance
+     */
+    public function plugin($name)
+    {
+        return jaxon()->plugin($name);
+    }
+
+    /**
+     * Register a specified Jaxon class.
+     *
+     * @param string            $sClassName             The name of the class to be registered
+     * @param array             $aOptions               The options to register the class with
+     *
+     * @return void
+     */
+    public function registerClass($sClassName, array $aOptions = array())
+    {
+        $this->_jaxonSetup();
+        jaxon()->registerClass($sClassName, $aOptions);
+    }
+
+    /**
+     * Get the javascript code to be sent to the browser.
+     *
+     * @return string  the javascript code
+     */
+    public function script($bIncludeJs = false, $bIncludeCss = false)
+    {
+        $this->_jaxonSetup();
+        return jaxon()->getScript($bIncludeJs, $bIncludeCss);
+    }
+
+    /**
+     * Get the HTML tags to include Jaxon javascript files into the page.
+     *
+     * @return string  the javascript code
+     */
+    public function js()
+    {
+        $this->_jaxonSetup();
+        return jaxon()->getJs();
+    }
+
+    /**
+     * Get the HTML tags to include Jaxon CSS code and files into the page.
+     *
+     * @return string  the javascript code
+     */
+    public function css()
+    {
+        $this->_jaxonSetup();
+        return jaxon()->getCss();
+    }
+
+    /**
+     * Set the init callback, used to initialise controllers.
+     *
+     * @param  callable         $callable               The callback function
+     * @return void
+     */
+    public function onInit($callable)
+    {
+        jaxon()->sentry()->setInitCallback($callable);
+    }
+
+    /**
+     * Set the pre-request processing callback.
+     *
+     * @param  callable         $callable               The callback function
+     * @return void
+     */
+    public function onBefore($callable)
+    {
+        jaxon()->sentry()->setBeforeCallback($callable);
+    }
+
+    /**
+     * Set the post-request processing callback.
+     *
+     * @param  callable         $callable               The callback function
+     * 
+     * @return void
+     */
+    public function onAfter($callable)
+    {
+        jaxon()->sentry()->setAfterCallback($callable);
+    }
+
+    /**
+     * Set the processing error callback.
+     *
+     * @param  callable         $callable               The callback function
+     * 
+     * @return void
+     */
+    public function onInvalid($callable)
+    {
+        jaxon()->sentry()->setInvalidCallback($callable);
+    }
+
+    /**
+     * Set the processing exception callback.
+     *
+     * @param  callable         $callable               The callback function
+     * 
+     * @return void
+     */
+    public function onError($callable)
+    {
+        jaxon()->sentry()->setErrorCallback($callable);
+    }
+
+    /**
+     * Check if the current request is a Jaxon request.
+     *
+     * @return boolean  True if the request is Jaxon, false otherwise.
+     */
+    public function canProcessRequest()
+    {
+        $this->_jaxonSetup();
+        return jaxon()->canProcessRequest();
+    }
+
+    /**
+     * Process the current Jaxon request.
+     *
+     * @return void
+     */
+    public function processRequest()
+    {
+        $this->_jaxonSetup();
+        // Process Jaxon Request
+        jaxon()->sentry()->processRequest();
+    }
+}
